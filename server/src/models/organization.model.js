fda0378cc3e140524be21668b1a880f2890c62b1
Dipesh <dipeshgavit333@gmail.com>
2025-02-18 22:20:09 +0530

    created Organization models

---
+++ server/src/models/organization.model.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+
+const Organization = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    logo: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    industry: {
+        type: String,
+        trim: true
+    },
+    size: {
+        type: String,
+        enum: ['1-10', '11-50', '51-200', '201-500', '500+'],
+        default: '1-10'
+    },
+    status: {
+        type: String,
+        rnum: ['active', 'inactive', 'suspemded'],
+        default: 'active'
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    subsritionPlan: {
+        type: String,
+        rnum: ['free', 'basic', 'professional', 'enterprise'],
+        default: 'free'
+    },
+    SubscriptionStatus: {
+        type: String,
+        enum: ['active', 'trial', 'expired'],
+        default: 'trial'
+    }
+
+}, {
+    timestamps: true
+})
