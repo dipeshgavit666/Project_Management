05d059c8c46a7ba8566c8014f8491d54435dc641
Dipesh <dipeshgavit333@gmail.com>
2025-02-18 21:15:55 +0530

    fixes in user.modeljs

---
+++ server/src/models/user.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose"
+
+const userSchema = new Schema({
+    FirstName: {
+        type: String,
+        required: [true, "First name is required"],
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last name is required"],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, "email is required"],
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, "password id required"],
+        minlength: [6, "Password must be at leaset 6 characters"]
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'project_manager', 'team_member'],
+        default: 'team_member'
+    },
+    organizationCreationPrivilege: {
+        type: Boolean,
+        default: false
+    },
+    avatar: {
+        type: String,
+        default: ""
+    },
+    defaultOrganization: {
+        type: Schema.Types.ObjectId,
+        ref: 'Organization'
+    },
+}, {
+    timestamps: true
+})
