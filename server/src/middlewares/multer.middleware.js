d996401590d02901514ba5c88378e38ccad9d2a2
Dipesh <dipeshgavit333@gmail.com>
2025-02-27 22:05:17 +0530

    implemented coudinary

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -1,1 +1,17 @@
 import multer from "multer"
+
+const storage = multer.diskStorage({
+    Destination: function (req, res, cb){
+        cb(null, './public/temp')
+
+    },
+    filename: function (req, res, cb){
+        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        // cb(null, file.fieldname + '-' + uniqueSuffix)
+        cb(null, file.originalname)
+    }
+})
+
+export const upload = multer({
+    storage
+})
---
+++ server/src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { log } from 'console';
+import fs from "fs"
+
+//configure cloudinary
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_SECRET
+});
+
+// Upload an image
+
+const UplaodOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        const response = await cloudinary.uploader.upload(
+            localFilePath, {
+                resource_type: "auto"
+            }
+        )
+        console.log(`File uplaoded on cloudinary. File src: ${response.url}`)
+        
+        //once the file is uploaded, ew would like to delete from our server
+        fs.unlinkSync(localFilePath)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export {UplaodOnCloudinary}
