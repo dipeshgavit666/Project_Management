bed427b8c78db7b93e5127ab4f5428eea914a9be
Dipesh <dipeshgavit333@gmail.com>
2025-03-20 01:36:18 +0530

    todo: update project routes

--- server/src/routes/project.routes.js
+++ server/src/routes/project.routes.js
@@ -13,7 +13,7 @@ import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 
 const router = Router()
-
+//these routes should be secure like invite routes or user routes, will update tommorrow
 router.route("/create-project").post(createProject)
 router.route("/update-project").patch(updateproject)
 router.route("/delete-project").delete(deleteProject)
