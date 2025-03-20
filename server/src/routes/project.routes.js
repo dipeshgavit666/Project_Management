b8bde211590b6f793425b6817e8904cb58d8fb8e
Dipesh <dipeshgavit333@gmail.com>
2025-03-20 16:26:41 +0530

    completed project controller and project route

--- server/src/routes/project.routes.js
+++ server/src/routes/project.routes.js
@@ -1,24 +1,19 @@
-import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
-import {Router} from "express"
+import { Router } from "express"
 import { 
     createProject,
-    updateproject,
+    updateProject,
     deleteProject,
     getAllProjects,
     getProject
- }  from "../controllers/project.controller.js";
+} from "../controllers/project.controller.js";
 import { verifyJWT } from '../middlewares/auth.middleware.js';
-
-
 
 const router = Router()
 
-router.route("/create-project").post(verifyJWT, createProject)
-router.route("/update-project").patch(verifyJWT, updateproject)
-router.route("/delete-project").delete(verifyJWT, deleteProject)
-router.route("/all-projects").get(verifyJWT, getAllProjects)
-router.route("get-project").get(verifyJWT, getProject)
-
+router.route("/").post(verifyJWT, createProject)
+router.route("/:projectId").patch(verifyJWT, updateProject)
+router.route("/:id").delete(verifyJWT, deleteProject)
+router.route("/").get(verifyJWT, getAllProjects)
+router.route("/:projectId").get(verifyJWT, getProject)
 
 export default router
